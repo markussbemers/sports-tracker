0fcebd94691256ee320ecd59ed02946339bb956c
markussWs <markuss.bemers@webscraper.io>
2022-07-01 04:05:12 +0300

    create-training

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\SportsTypeController;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\OrganizationController;
@@ -33,24 +32,20 @@ Route::get('/teams', function () {
     return view('teams');
 })->middleware(['auth'])->name('teams');
 
-Route::resource('organizations', OrganizationController::class);
-Route::resource('create_organization', OrganizationController::class);
-Route::resource('create_team', TeamController::class);
-
 Route::get('organizations', [OrganizationController::class, 'index'])->middleware(['auth'])->name('organizations');
 Route::get('teams/organizations/{id}', [TeamController::class, 'getTeamsByOrganizations'])->middleware(['auth'])->name('teams/organizations/{id}');
 
-Route::get('/trainings', [TrainingController::class, 'index'])->middleware(['auth'])->name('trainings');
-
-Route::get('sports-types', [SportsTypeController::class, 'index'])->middleware(['auth'])->name('sports_types');
-Route::get('sports-types/{id}', [TeamController::class, 'getTeamsBySportsType'])->middleware(['auth'])->name('sports-types/{id}');
-
+Route::resource('create_organization', OrganizationController::class);
 Route::get('create_organization', [OrganizationController::class, 'create'])->middleware(['auth'])->name('create_organization');
 
 Route::resource('create_team', TeamController::class);
 Route::get('create_team', [TeamController::class, 'create'])->middleware(['auth'])->name('create_team');
 
-Route::get('lang/{locale}',LanguageController::class);
+Route::get('edit_team/{id}/{message?}/{message2?}', [TeamController::class, 'edit'])->middleware(['auth'])->name('edit_team');
+Route::post('add_palyer', [TeamController::class, 'addPlayer']);
+Route::post('destroy_player/{id}/{team_id?}', [TeamController::class, 'destroyPlayer']);
+Route::post('change_coach', [TeamController::class, 'changeCoach']);
+Route::post('add_training', [TeamController::class, 'addTraining']);
 
 require __DIR__.'/auth.php';
 
